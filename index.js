607ecdc716076eb7dff53a9a2a43275076fae0f8
vassiliy278 <vasyl.koval@pm.bet>
2022-02-12 13:34:46 +0200

    oferta

--- index.js
+++ index.js
@@ -10,28 +10,44 @@ const buttonInc = document.getElementById('itemplus')
 const buttonDec = document.getElementById('itemminus')
 const amountValue = document.querySelector('.amount_value')
 let price = 275
+const gameContainer = document.querySelector('.game_container')
+
 function changeCountValue(step) {
     if(step == '-'){
+        gameContainer.lastElementChild.remove()
         countValue -= 1
         if(countValue == 1) {
             this.setAttribute('disabled', 'disabled')
             btnValue.innerText = countValue
         } else {
             this.removeAttribute('disabled')
+            buttonInc.removeAttribute('disabled')
+            
         }
         btnValue.innerText = countValue
     } else if(step == '+') {
         countValue += 1
         if(countValue > 1) buttonDec.removeAttribute('disabled')
+        if (countValue == 10) {
+            this.setAttribute('disabled', 'disabled')
+        }
         btnValue.innerText = countValue
+        gameContainer.insertAdjacentHTML('afterbegin', `<img src="./img/game.svg" alt="" class="game_img">`)
+
     }
     if(countValue >= 4) {
+        document.querySelectorAll('.game_img').forEach(e => {
+            e.classList.add('game_img_many')
+        })
         price=250
         amountValue.innerText = countValue*price
         amountValue.nextElementSibling.style.color = 'rgb(45, 205, 116)'
         amountValue.style.color = 'rgb(45, 205, 116)'
         
     } else {
+        document.querySelectorAll('.game_img').forEach(e => {
+            e.classList.remove('game_img_many')
+        })
         price=275
         amountValue.innerText = countValue*price
         amountValue.nextElementSibling.style.color = 'inherit'
@@ -68,7 +84,10 @@ form.addEventListener('input', function(e){
         }
         postData[e.target.name] = e.target.value
     if (this.checkValidity()) {
-        btnContainer.innerHTML = `<button type="submit"  class="spinner" style="width: 150px; height: 50px; margin-top:30px" disabled>Загрузка...</button> `
+        btnContainer.innerHTML = `                <div class="spinner" >
+        <img src="./img/game.svg" alt="" class="game_img ">
+        <div class="load_text" style="position: absolute;">загрузка...</div>
+    </div>`
 
         const {count, mount, city, warehouse, phone, firstName, lastName} = postData
         fetch(`https://liqpayvasya.herokuapp.com/pay/${count}/${mount}/${city}/${warehouse}/${firstName}/${lastName}/${phone}`)
